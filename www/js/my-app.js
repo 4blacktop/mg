9224ff9787005970a8e60854ed4d90f1df43769e
4blacktop <4blogga@gmail.com>
2016-02-01 18:21:54 +1000

    1822

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -9,8 +9,8 @@ var myApp = new Framework7({
 	
 // Export selectors engine
 var $$ = Dom7;
-var jsonURL = 'http://scr.ru/mg/www/php/json680000.txt';
-// var jsonURL = 'http://27podarkov.ru/json680000.txt';
+// var jsonURL = 'http://scr.ru/mg/www/php/json680000.txt';
+var jsonURL = 'http://27podarkov.ru/json680000.txt';
 
 // Ajax setting for timeout
 $$.ajaxSetup({
