4062ce338a862af8b69320f3ab2fc33879d946b6
4blacktop <4blogga@gmail.com>
2016-02-06 20:57:00 +1000

    2056

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -147,77 +147,109 @@ function selectPicture() {
 		},
 		{ quality: 50, destinationType: navigator.camera.DestinationType.FILE_URI, sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY});
 };*/
+ /**
+     * Take picture with camera
+     */
+    function takePicture() {
+        navigator.camera.getPicture(
+            function(uri) {
+                var img = document.getElementById('camera_image');
+                img.style.visibility = "visible";
+                img.style.display = "block";
+                img.src = uri;
+                document.getElementById('camera_status').innerHTML = "Success";
+            },
+            function(e) {
+                console.log("Error getting picture: " + e);
+                document.getElementById('camera_status').innerHTML = "Error getting picture.";
+            },
+            { quality: 50, destinationType: navigator.camera.DestinationType.FILE_URI});
+    };
 
-/**  * Upload current picture  */
-function uploadPicture() {
-	
-	// Get URI of picture to upload
-	var img = document.getElementById('camera_image');
-	var imageURI = img.src;
-	if (!imageURI || (img.style.display == "none")) {
-		document.getElementById('camera_status').innerHTML = "Сделайте фотографию или выберите из списка.";
-		return;
-	}
-	
-	// Verify server has been entered
-	server = document.getElementById('serverUrl').value;
-	if (server) {
-		
-		// Specify transfer options
-		var options = new FileUploadOptions();
-		options.fileKey="file";
-		options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
-		options.mimeType="image/jpeg";
-		options.chunkedMode = false;
+    /**
+     * Select picture from library
+     */
+    function selectPicture() {
+        navigator.camera.getPicture(
+            function(uri) {
+                var img = document.getElementById('camera_image');
+                img.style.visibility = "visible";
+                img.style.display = "block";
+                img.src = uri;
+                document.getElementById('camera_status').innerHTML = "Success";
+            },
+            function(e) {
+                console.log("Error getting picture: " + e);
+                document.getElementById('camera_status').innerHTML = "Error getting picture.";
+            },
+            { quality: 50, destinationType: navigator.camera.DestinationType.FILE_URI, sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY});
+    };
+    
+    /**
+     * Upload current picture
+     */
+    function uploadPicture() {
+    	
+    	// Get URI of picture to upload
+        var img = document.getElementById('camera_image');
+        var imageURI = img.src;
+        if (!imageURI || (img.style.display == "none")) {
+            document.getElementById('camera_status').innerHTML = "Take picture or select picture from library first.";
+            return;
+        }
+        
+        // Verify server has been entered
+        server = document.getElementById('serverUrl').value;
+        if (server) {
+        	
+            // Specify transfer options
+            var options = new FileUploadOptions();
+            options.fileKey="file";
+            options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+            options.mimeType="image/jpeg";
+            options.chunkedMode = false;
 
-		// Transfer picture to server
-		var ft = new FileTransfer();
-		ft.upload(imageURI, server, function(r) {
-			document.getElementById('camera_status').innerHTML = "Загрузка ОК: "+r.bytesSent+" байт отправлено.";  
-			document.getElementById("newstext").value = '';   // clear news text data    
-			var img = document.getElementById('camera_image'); // clear photo thumbnail  
-			img.style.visibility = "hidden";
-			img.style.display = "none";			
-			myApp.alert( "Новость загружена.<br />Держите нас в курсе!", 'Благодарим '+r.bytesSent+" раз!", function () {
-			// $$(".back").click();
-			
-			});
-		}, function(error) {
-			document.getElementById('camera_status').innerHTML = "Ошибка: Code = "+error.code;myApp.alert( "Ошибка: Code = "+error.code, 'Ошибка!', function () {
-			});            	
-		}, options);
-	}
-}
+            // Transfer picture to server
+            var ft = new FileTransfer();
+            ft.upload(imageURI, server, function(r) {
+                document.getElementById('camera_status').innerHTML = "Upload successful: "+r.bytesSent+" bytes uploaded.";            	
+            }, function(error) {
+                document.getElementById('camera_status').innerHTML = "Upload failed: Code = "+error.code;            	
+            }, options);
+        }
+    }
 
-/**  * View pictures uploaded to the server  */
-function viewUploadedPictures() {
-	
-	// Get server URL
-	server = document.getElementById('serverUrl').value;
-	if (server) {
-		
-		// Get HTML that lists all pictures on server using XHR	
-		var xmlhttp = new XMLHttpRequest();
+    /**
+     * View pictures uploaded to the server
+     */
+    function viewUploadedPictures() {
+    	
+    	// Get server URL
+        server = document.getElementById('serverUrl').value;
+        if (server) {
+        	
+            // Get HTML that lists all pictures on server using XHR	
+            var xmlhttp = new XMLHttpRequest();
 
-		// Callback function when XMLHttpRequest is ready
-		xmlhttp.onreadystatechange=function(){
-			if(xmlhttp.readyState === 4){
+            // Callback function when XMLHttpRequest is ready
+            xmlhttp.onreadystatechange=function(){
+                if(xmlhttp.readyState === 4){
 
-				// HTML is returned, which has pictures to display
-				if (xmlhttp.status === 200) {
-					document.getElementById('server_images').innerHTML = xmlhttp.responseText;
-				}
+                    // HTML is returned, which has pictures to display
+                    if (xmlhttp.status === 200) {
+                    	document.getElementById('server_images').innerHTML = xmlhttp.responseText;
+                    }
 
-				// If error
-				else {
-					document.getElementById('server_images').innerHTML = "Не удалось загрузить изображение с сервера.";
-				}
-			}
-		};
-		xmlhttp.open("GET", server , true);
-		xmlhttp.send();       	
-	}	
-}
-
+                    // If error
+                    else {
+                    	document.getElementById('server_images').innerHTML = "Error retrieving pictures from server.";
+                    }
+                }
+            };
+            xmlhttp.open("GET", server , true);
+            xmlhttp.send();       	
+        }	
+    }
+    
 
   
